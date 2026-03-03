1ff969d4dc7281818dc36d906b81e2b245882354
EugeneHard-cmd <eugenehard69@gmail.com>
2026-03-03 22:09:47 +0100

    Made fourth task

--- Homework2.py
+++ Homework2.py
@@ -29,3 +29,16 @@ amount_of_minutes = int(input("Enter how many minutes: "))
 amount_of_hours = amount_of_minutes // 60
 amount_of_minutes_left = amount_of_minutes % 60
 print(f"{amount_of_hours} hours and {amount_of_minutes_left} minutes")
+
+# 4. “Розрахунок знижки”
+# Користувач вводить ціну товару і розмір знижки у %.
+# Програма виводить кінцеву ціну.
+# Приклад:
+# Введіть ціну: 1000
+# Введіть знижку (%): 15
+# Ціна зі знижкою: 850.0
+
+price = int(input("Enter a price: "))
+discount = int(input("Enter a discount: "))
+result = price - ((price * discount) / 100)
+print(f"Answer is: {result}")
