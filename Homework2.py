084e9ac31e502a235d64bd148a2218f9566c7889
EugeneHard-cmd <eugenehard69@gmail.com>
2026-03-03 22:01:44 +0100

    Made 2 tasks out of 6

---
+++ Homework2.py
@@ -0,0 +1,26 @@
+# 1. Квадрат числа.
+# Користувач вводить число. Програма виводить його квадрат.
+# Приклад:
+# Введіть число: 5
+# Квадрат числа: 25
+
+number = int(input("Enter a number: "))
+print(f"Answer is: {number**2}")
+
+# 2. “Середнє трьох чисел”
+# Програма запитує три числа і виводить їх середнє арифметичне.
+# Приклад:
+# Введіть три числа: 2, 4, 6
+# Середнє: 4.0
+
+first_number = int(input("Enter a first number: "))
+second_number = int(input("Enter a second number: "))
+third_number = int(input("Enter a third number: "))
+result = (first_number + second_number + third_number) / 3
+print(f"Answer is: {result}")
+
+# 3. “Перетворення хвилин у години”
+# Користувач вводить кількість хвилин. Програма виводить, скільки це годин і хвилин.
+# Приклад:
+# Введіть кількість хвилин: 135
+# 2 години 15 хвилин
