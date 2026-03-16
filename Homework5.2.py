40d315cf76089cf44f0e2b4a7f8270bb368dde48
EugeneHard-cmd <eugenehard69@gmail.com>
2026-03-16 22:02:07 +0100

    Made task

---
+++ Homework5.2.py
@@ -0,0 +1,31 @@
+while True:
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+
+    users_operation = int(input("Make a choice of the mathematical operation:\n"
+                             "1 - Addition\n"
+                             "2 - Subtraction\n"
+                             "3 - Multiplication\n"
+                             "4 - Division\n"))
+    match users_operation:
+        case 1:
+            print(first_number + second_number)
+
+        case 2:
+            print(first_number - second_number)
+        case 3:
+            print(first_number * second_number)
+        case 4:
+            if second_number == 0:
+                print("Enter a correct number")
+            else:
+                print(first_number / second_number)
+        case _:
+            print("Enter a correct number")
+
+    users_choice = input("Do you want to continue? (yes/y): ").lower()
+
+    if users_choice != "yes" and users_choice != "y":
+        print("Ok. Goodbye!")
+        break
+
